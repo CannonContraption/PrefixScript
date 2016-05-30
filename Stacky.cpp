5ea38da261c77d6d5ffefe6bffa5fe59cd60098d
Jim Read <jimmydean886@hotmail.com>
2016-05-30 13:45:53 -0400

    Fixes Stacky.cpp regression caused by attempt to rewrite the STD::STACK library Also enhances the readme

--- Stacky.cpp
+++ Stacky.cpp
@@ -1,7 +1,15 @@
-template <class T> class Stacky{
-protected:
-    T storage[5];
-    T *backup;
+#include<stack>
+using namespace std;
+template <class T>
+class Stacky: public stack<T> {
 public:
-    
+    T pop(){
+        if(!this->empty()){
+            T number = this->top();
+            stack<T>::pop();
+            return number;
+        } else {
+            return -666;
+        }
+    }
 };
--- doc.js
+++ doc.js
@@ -1,4 +1,4 @@
-versionminor = 14;
+versionminor = 15;
 versionmajor = 0;
 version = versionmajor + (versionminor / 1000.0);
 document.getElementById("introduction").innerHTML="PrefixScript "+version+" Documentation Page";
