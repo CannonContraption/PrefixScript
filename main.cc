5e75988fb59b8777ec2ed9dbe79ed318a8b9f11f
Jim Read <jimmydean886@hotmail.com>
2016-09-23 23:36:22 -0400

    Fixed main.cc deps
    Main.cc now correctly calls commandparser again.
    
    It also no longer references the obsoleted Stacky.cpp file, and
    will build properly again. Its build output is still useless,
    however.

--- main.cc
+++ main.cc
@@ -1,9 +1,7 @@
-#include "Stacky.cpp"
 #include "commandregister/commandregister.cc"
 #include "commandparser/commandparser.cc"
 
 int main(int argc, char* argv[]){
 	commandregister reg;
-	commandparser   prs;
-	prs.main();
+	commandparser   prs (&reg);
 }
