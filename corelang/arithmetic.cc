c5dffd1ff67a3f688ecac7dcff12c6d8c12d1660
Jim Read <jimcread@gmail.com>
2016-10-21 15:59:49 -0400

    Added stack commands to arithmetic
    The arithmetic functions can now handle the pop and top stack
    commands.

--- corelang/arithmetic.cc
+++ corelang/arithmetic.cc
@@ -3,7 +3,13 @@ bool add(){
 	string input;
 	for(;;){
 		input=prs.read();
-		if(input == "/+"){
+		if(input == "pop"){
+			sum += prs.pop();
+		}
+		else if(input == "top"){
+			sum += prs.top();
+		}
+		else if(input == "/+"){
 			prs.store(sum);
 			return true;
 		}
@@ -21,11 +27,24 @@ bool sub(){
 		difference=0;
 	}
 	else{
-		difference = prs.todouble(input);
+		if(input == "pop"){
+			difference = prs.pop();
+		}
+		else if(input == "top"){
+			difference = prs.top();
+		}
+		else{
+			difference = prs.todouble(input);
+		}
 		input = prs.read();
 	}
 	while(input != "/-"){
-		difference -= prs.todouble(input);
+		if(input == "pop"){
+			difference -= prs.pop();
+		} else if (input == "top"){
+			difference -= prs.top();
+		}
+		else difference -= prs.todouble(input);
 		input = prs.read();
 	}
 	prs.store(difference);
@@ -37,7 +56,9 @@ bool mul(){
 	string input;
 	for(;;){
 		input = prs.read();
-		if(input == "/*"){
+		if(input == "pop") product *= prs.pop();
+		else if (input == "top") product *= prs.top();
+		else if(input == "/*"){
 			prs.store(product);
 			return true;
 		}
@@ -55,11 +76,15 @@ bool div(){
 		result = 0;
 	}
 	else {
-		result = prs.todouble(input);
+		if(input == "pop") result = prs.pop();
+		else if(input == "top") result = prs.top();
+		else result = prs.todouble(input);
 		input = prs.read();
 	}
 	while(input != "//"){
-		result /= prs.todouble(input);
+		if(input == "pop") result /= prs.pop();
+		else if (input == "top") result /= prs.top();
+		else result /= prs.todouble(input);
 		input = prs.read();
 	}
 	prs.store(result);
