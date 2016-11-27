db29b8032ad6588acf1c7c2c52f1a125b197459a
Jim Read <jimmydean886@hotmail.com>
2016-11-27 01:28:56 -0500

    Added POC documentation to stringmanagement.cc
    Added plain-old-comment documentation to the string management
    portion of the interpreter.

--- corelang/stringmanagement.cc
+++ corelang/stringmanagement.cc
@@ -1,9 +1,22 @@
+/*
+ * inputstring()
+ *
+ * function run on sinput command
+ * reads next string from cin whether or not we're reading from a script and puts it in
+ * the string stack. Useful for user input.
+ */
 bool inputstring(){
 	string input;
 	cin>>input;
 	prs.strstore(input);
 }
 
+/*
+ * stringpop()
+ *
+ * function run on strpop command
+ * prints the top string and pops it from the string stack
+ */
 bool stringpop(){
 	if(!prs.strempty()){
 		cout<<prs.strpop()<<endl;
@@ -13,6 +26,12 @@ bool stringpop(){
 	}
 }
 
+/*
+ * stringpopn()
+ *
+ * function run on strpopn command
+ * prints the top string and pops it from the stack, printing a newline after the fact.
+ */
 bool stringpopn(){
 	if(!prs.strempty())
 		cout<<prs.strpop()<<endl;
@@ -20,6 +39,12 @@ bool stringpopn(){
 		cerr<<"\033[1;31mERROR: \033[m\033[31mString stack is empty!\033[m"<<endl;
 }
 
+/*
+ * strtop()
+ *
+ * function run on strtop command
+ * Prints the top string on the string stack
+ */
 bool strtop(){
 	if(!prs.strempty())
 		cout<<prs.strtop();
@@ -27,6 +52,12 @@ bool strtop(){
 		cerr<<"\033[1;31mERROR: \033[m\033[31mString stack is empty!\033[m"<<endl;
 }
 
+/*
+ * strtopn()
+ *
+ * function run on strtopn command
+ * Prints thetop string on the string stack followed by a line break
+ */
 bool strtopn(){
 	if(!prs.strempty())
 		cout<<prs.strtop()<<endl;
