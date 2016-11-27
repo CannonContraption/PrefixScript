15d1f687c9f9a785cc00685065adaa061a56c0ee
Jim Read <jimmydean886@hotmail.com>
2016-11-27 01:16:31 -0500

    Added POC documentation to corelang.cc
    Added plain-old-comment documentation to the corelang.cc file,
    allowing a casual onlooker to understand what each function does
    despite their names differing from interpreted commands.

--- corelang/corelang.cc
+++ corelang/corelang.cc
@@ -1,3 +1,9 @@
+/*
+ * commentsearch()
+ *
+ * function run when the c command is executed
+ * prevents the interpreter from doing anything until it sees /c
+ */
 bool commentsearch(){
 	string command;
 	while(command != "/c"){
@@ -5,6 +11,13 @@ bool commentsearch(){
 	}
 }
 
+/*
+ * printtoscreen()
+ *
+ * command run when the print command is executed
+ * prints everything it reads until it sees /print
+ * includes some workarounds for PrefixScript's more peculiar tendancies
+ */
 bool printtoscreen(){
 	string command = prs.read();
 	string toprint = "";
@@ -31,16 +44,37 @@ bool printtoscreen(){
 	cout<<toprint;
 }
 
+/*
+ * inputdouble()
+ *
+ * function run on input command
+ * searches cin for a double whether or not we are reading from a script.
+ * useful for when we want user input.
+ */
 bool inputdouble(){
 	double input;
 	cin>>input;
 	prs.store(input);
 }
 
+/*
+ * newline()
+ *
+ * function run on linebreak command
+ * adds a line break.
+ */
 bool newline(){
 	cout<<endl;
 }
 
+/*
+ * vcheck()
+ * 
+ * old version-checking code
+ * used to ensure script compatiblity during developement as a sort of
+ * check to make sure that the code had been modified when the version
+ * number went up
+ */
 bool vcheck(){
 	string input;
 	cin >> input;
@@ -49,6 +83,13 @@ bool vcheck(){
 	cout<<"VCheck is broken. Results are not available."<<endl;
 }
 
+/*
+ * programreturn()
+ *
+ * function run on return command
+ * causes the interpreter to return with the next entered number as the
+ * return code.
+ */
 bool programreturn(){
 	string input = prs.read();
 	int code = prs.toint(input);
