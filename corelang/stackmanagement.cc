3e21ad146108ea31b59957e9b4201ef144602cf1
Jim Read <jimmydean886@hotmail.com>
2016-11-27 01:23:27 -0500

    Added POC documentation to stackmanagement.cc
    Added plain-old-comment documentation describing the functions in
    the stack management portion of PrefixScript

--- corelang/stackmanagement.cc
+++ corelang/stackmanagement.cc
@@ -1,13 +1,32 @@
+/*
+ * stack_pop()
+ * 
+ * function run on pop command
+ * basically a wrapper for prs.pop(), pops the top value and displays it
+ */
 bool stack_pop(){
 	if(!prs.empty())
 		cout<<prs.pop();
 	else
 		prs.pop();
 }
+
+/*
+ * stack_silentpop()
+ *
+ * function run on silentpop command
+ * pops the top value without displaying it
+ */
 bool stack_silentpop(){
 	prs.pop();
 }
 
+/*
+ * stack_popn()
+ *
+ * function run on popn command
+ * like stack_pop but adds a line break
+ */
 bool stack_popn(){
 	if(!prs.empty())
 		cout<<prs.pop()<<endl;
@@ -15,6 +34,12 @@ bool stack_popn(){
 		prs.pop();
 }
 
+/*
+ * stack_top()
+ *
+ * function run on the top command
+ * displays the top number on the current stack
+ */
 bool stack_top(){
 	if(!prs.empty())
 		cout<<prs.top();
@@ -22,6 +47,12 @@ bool stack_top(){
 		prs.top();
 }
 
+/*
+ * stack_topn()
+ *
+ * function run on the topn command
+ * displays top number followed by newline
+ */
 bool stack_topn(){
 	if(!prs.empty())
 		cout<<prs.top()<<endl;
@@ -29,6 +60,13 @@ bool stack_topn(){
 		prs.top();
 }
 
+/*
+ * switchstack()
+ *
+ * function run on stack command
+ * takes next input and switches to the stack with that number.
+ * If no such stack exists, it screams profanity gently at the user.
+ */
 bool switchstack(){
 	string input = prs.read();
 	int stacknum = prs.toint(input);
