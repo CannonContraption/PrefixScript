b76cc87668d5151718e789a1a5dce3ba75507fdd
Jim Read <jimmydean886@hotmail.com>
2016-11-26 20:39:53 -0500

    Documented inside of headers
    Headers for the command register and command parser have been
    documented through comments in a more complete way than before.
    
    (AKA they now are commented, allowing for someone to read the code
    later)

--- commandparser/commandparser.hh
+++ commandparser/commandparser.hh
@@ -3,40 +3,71 @@
 #include <sstream>
 
 class commandparser{
+	//We're using the same command register everywhere, so we want
+	//to use a pointer in order to prevent the program from copying
+	//the one we already have.
 	commandregister   *cmdreg;
 protected:
+	/*
+	 * basic common variables
+	 * 
+	 * Stuff that's common to most commands and used often
+	 */
 	string            command;
+	/*
+	 * File-read stuff
+	 * 
+	 * stdin vs. file stuff
+	 */
 	bool              isfile;
 	string            filename;
-	vector<string>    repetitionbuffer[32];
-	int               repeatlevel; //Stacks, so nested loops work
+	bool              scriptread;
+	fstream           scriptfile;
+	bool              openfile();
+	/*
+	 * Storage commands
+	 * 
+	 * Used for storing values. Also variables to select which
+	 * storage stack to use
+	 */
 	stack<double>     storage[3];
 	stack<string>     strstorage;
 	int               currentstorage;
+	//Old fail count code, need to check if this is still used
 	int               failcount;
 	int               maxfails;
+	/*
+	 * File-based repetition variables, allowing for 32 nested
+	 * loops controlled by start and end line numbers.
+	 */
 	int               repeatlower[32];
 	int               repeatupper[32];
 	char              repeatmode[32];
-	double            memory[32];
 	int               repeatindex;
 	bool              repeatread;
-	bool              scriptread;
+	vector<string>    repetitionbuffer[32];
+	int               repeatlevel; //Stacks, so nested loops work
+	/*
+	 * Memory
+	 * 
+	 * 32 stack-independant memory slots
+	 */
+	double            memory[32];
 	int               returncode;
 	bool              exitnow;
-	
-	fstream           scriptfile;
-	
-	bool              openfile();
 public:
+	/*
+	 * Global commands
+	 * 
+	 * things like reading commands, and stack management
+	 */
 	string            read();
-	bool              setfilename(string file);
-	void              store(double value);
-	void              strstore(string value);
 	string            strtop();
 	string            strpop();
 	double            pop();
 	double            top();
+	void              store(double value);
+	void              strstore(string value);
 	bool              empty();
 	bool              strempty();
 	int               listen();
@@ -44,6 +75,20 @@ public:
 	double            todouble(const string& s);
 	double            todouble_multsafe(const string& s);
 	int               toint(const string& s);
+	/*
+	 * File io commands
+	 * 
+	 * for setting file mode, and for reading from a file
+	 */
+	bool              setfilename(string file);
+	/*
+	 * Constructors and exit commands
+	 * 
+	 * Defines headers for constructors and exit commands.
+	 * This is so we can actually get a hold of our command
+	 * register and keep it common with the rest of the 
+	 * program.
+	 */
 	commandparser     (commandregister * r);
 	void              exitprogram(int rcd);
 };
--- commandregister/commandregister.hh
+++ commandregister/commandregister.hh
@@ -3,12 +3,32 @@
 using namespace std;
 class commandregister{
 protected:
+	/*
+	 * The commandmodule struct is the basis of the command
+	 * register. It acts as the central object by which
+	 * commands are searched for. When a person types a string,
+	 * the corresponding function is executed. This model has
+	 * the advantage of control, where it is possible to check
+	 * to make sure no two commands have the same name. While
+	 * this checking happens at runtime, not compile-time, it
+	 * still is better than no checking, like before.
+	 */
 	struct commandmodule{
 		bool (*function)();
 		string commandname;
 	};
+	/*
+	 * Because I'm lazy, these commands are put in a vector
+	 * instead of something which may be more efficient, like
+	 * a linked list.
+	 */
 	vector<commandmodule> commands;
 public:
+	/*
+	 * The command register, being very basic, requires no more
+	 * than a check, run, and insert command. It does no more
+	 * than just store the commands we have at our disposal.
+	 */
 	bool insertcommand(string command, bool(*function)());
 	bool checkforcommand(string command);
 	bool runcommand(string command);
