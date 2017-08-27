d30d85f9ea50a5d7c9ec46bcbd4173baf1fc6b87
Jim Read <jimmydean886@hotmail.com>
2017-08-27 11:49:19 -0400

    Changed indent style of main.cc
    Changed to the Emacs indent style.

--- main.cc
+++ main.cc
@@ -45,35 +45,35 @@ directory of the source tree. There each command and its functions are described
 */
 
 int main(int argc, char* argv[]){
-	if(argc>1){
-		string file = argv[1];
-        prs.setfilename(file);
-	}
-	reg.insertcommand("+",        add);
-	reg.insertcommand("-",        sub);
-	reg.insertcommand("*",        mul);
-	reg.insertcommand("/",        div);
-	reg.insertcommand("pop",      stack_pop);
-	reg.insertcommand("popn",     stack_popn);
-	reg.insertcommand("silentpop",stack_silentpop);
-	reg.insertcommand("top",      stack_top);
-	reg.insertcommand("topn",     stack_topn);
-	reg.insertcommand("stack",    switchstack);
-	reg.insertcommand("c",        commentsearch);
-	reg.insertcommand("print",    printtoscreen);
-	reg.insertcommand("input",    inputdouble);
-	reg.insertcommand("sinput",   inputstring);
-	reg.insertcommand("linebreak",newline);
-	reg.insertcommand("strpop",   stringpop);
-	reg.insertcommand("strpopn",  stringpopn);
-	reg.insertcommand("strtop",   strtop);
-	reg.insertcommand("strtopn",  strtopn);
-	reg.insertcommand("vcheck",   vcheck);
-	reg.insertcommand("return",   programreturn);
-	reg.insertcommand("if",       ifstatement);
-	reg.insertcommand("memstore", memstore);
-	reg.insertcommand("memrecall",memrecall);
-	reg.insertcommand("math",     advancedmath);
-	reg.insertcommand("string",   codestring);
-	return prs.listen();
+  if(argc>1){
+    string file = argv[1];
+    prs.setfilename(file);
+  }
+  reg.insertcommand("+",        add);
+  reg.insertcommand("-",        sub);
+  reg.insertcommand("*",        mul);
+  reg.insertcommand("/",        div);
+  reg.insertcommand("pop",      stack_pop);
+  reg.insertcommand("popn",     stack_popn);
+  reg.insertcommand("silentpop",stack_silentpop);
+  reg.insertcommand("top",      stack_top);
+  reg.insertcommand("topn",     stack_topn);
+  reg.insertcommand("stack",    switchstack);
+  reg.insertcommand("c",        commentsearch);
+  reg.insertcommand("print",    printtoscreen);
+  reg.insertcommand("input",    inputdouble);
+  reg.insertcommand("sinput",   inputstring);
+  reg.insertcommand("linebreak",newline);
+  reg.insertcommand("strpop",   stringpop);
+  reg.insertcommand("strpopn",  stringpopn);
+  reg.insertcommand("strtop",   strtop);
+  reg.insertcommand("strtopn",  strtopn);
+  reg.insertcommand("vcheck",   vcheck);
+  reg.insertcommand("return",   programreturn);
+  reg.insertcommand("if",       ifstatement);
+  reg.insertcommand("memstore", memstore);
+  reg.insertcommand("memrecall",memrecall);
+  reg.insertcommand("math",     advancedmath);
+  reg.insertcommand("string",   codestring);
+  return prs.listen();
 }
